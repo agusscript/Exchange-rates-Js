64561ca6d4b16c1db830a290f15cf51ec3e299c3
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-17 20:56:06 -0300

    Add function to get base currency

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ const $inputSelectMonth = document.querySelector("#month");
 const $inputSelectDay = document.querySelector("#day");
 const currentDate = new Date().toJSON().slice(0, 10);
 const currenttYear = currentDate.slice(0, 4);
+const $currencySelect = document.querySelector("#currency-select");
 
 function getOptionsDate(min, max, select) {
   for (let i = max; i >= min; i--) {
@@ -23,14 +24,23 @@ getOptionsDate(2000, currenttYear, $inputSelectYear);
 getOptionsDate(1, 12, $inputSelectMonth);
 getOptionsDate(1, 31, $inputSelectDay);
 
-document.querySelector("#show").onclick = () => {
-  let selectedDate = `${$inputSelectYear.value}-${$inputSelectMonth.value}-${$inputSelectDay.value}`;
+function getBaseCurrency(currency) {
+  return `?base=${currency}`;
+}
 
-  fetch(requestApiURL + selectedDate)
+function getData(date, currency) {
+  fetch(requestApiURL + date + currency)
     .then((response) => response.json())
     .then((responseJSON) => {
       console.log(responseJSON);
     });
+}
+
+document.querySelector("#show").onclick = () => {
+  let selectedDate = `${$inputSelectYear.value}-${$inputSelectMonth.value}-${$inputSelectDay.value}`;
+  let selectedBaseCurrency = getBaseCurrency($currencySelect.value);
+
+  getData(selectedDate, selectedBaseCurrency);
 
   event.preventDefault();
 };
