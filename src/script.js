3879f57ab3111e24ed747be74b44b2514f9dc879
Agustin Sanchez <agus.sanchez.240@gmail.com>
2023-03-24 13:34:58 -0300

    Add functions to manage ui events

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 const requestApiURL = "https://api.exchangerate.host/";
+const ratesForm = document.querySelector(".form-rates-section");
+const convertForm = document.querySelector(".form-convert-section");
+const overlay = document.querySelector(".overlay");
 
 function manageRatesData() {
   const $currencySelect = document.querySelector("#currency-select");
@@ -18,17 +21,19 @@ function showRates(data) {
   let $tableCurrency = document.querySelector("table");
   let $titleRatesTable = document.querySelector("caption");
   $titleRatesTable.textContent = `Exchange rates for "${data.base}" in date: ${data.date}`;
+
   Object.keys(data.rates).forEach((currency) => {
     let currencyTableRow = document.createElement("tr");
-    currencyTableRow.classList.add("currency-row");
     let currencyCell = document.createElement("td");
     let valueCell = document.createElement("td");
     currencyCell.textContent = currency;
     valueCell.textContent = data.rates[currency];
-    currencyTableRow.append(currencyCell, valueCell); 
+    currencyTableRow.append(currencyCell, valueCell);
     $tableCurrency.append(currencyTableRow);
-    $tableCurrency.style.display = "block";
   });
+
+  $tableCurrency.style.display = "block";
+  document.querySelector(".form-rates-section").style.height = "100vh";
 }
 
 function validateDate() {
@@ -49,10 +54,8 @@ function manageConvertCurrenciesData() {
   const $inputFromCurrency = document.querySelector("#from-currency");
   const $inputToCurrency = document.querySelector("#to-currency");
   const $inputAmount = document.querySelector("#amount");
-  let selectedCurrenciesToConvert = getCurrencysToConvert(
-    $inputFromCurrency.value,
-    $inputToCurrency.value
-  );
+  let selectedCurrenciesToConvert = 
+  getCurrencysToConvert($inputFromCurrency.value, $inputToCurrency.value);
   let selectedAmount = getAmount($inputAmount.value);
 
   fetch(requestApiURL + selectedCurrenciesToConvert + selectedAmount)
@@ -64,8 +67,9 @@ function manageConvertCurrenciesData() {
 }
 
 function showConvertResult(data) {
-  let resultText = document.querySelector("h2");
-  resultText.textContent = `${data.result} ${data.query.to}`;
+  let resultText = document.querySelector(".result-convert");
+  resultText.textContent = 
+  `${data.query.amount} ${data.query.from} = ${data.result} ${data.query.to}`;
 }
 
 function getCurrencysToConvert(currency1, currency2) {
@@ -76,13 +80,27 @@ function getAmount(amount) {
   return `&amount=${amount}`;
 }
 
-document.querySelector("#show").onclick = () => {
-  manageRatesData();
-  event.preventDefault();
+document.querySelector("#see-rates-btn").onclick = () => {
+  ratesForm.style.left = "50%";
+  overlay.style.display = "block";
 };
 
-document.querySelector("#remove").onclick = () => {
-  location.reload();
+document.querySelector("#convert-btn").onclick = () => {
+  convertForm.style.left = "50%";
+  overlay.style.display = "block";
+};
+
+document.querySelectorAll(".close-menu-img").forEach((e) => {
+  e.onclick = () => {
+    ratesForm.style.left = "-100%";
+    convertForm.style.left = "-100%";
+    overlay.style.display = "none";
+  };
+});
+
+document.querySelector("#show").onclick = () => {
+  manageRatesData();
+  ratesForm.querySelector(".form-rates").style.display = "none";
   event.preventDefault();
 };
 
